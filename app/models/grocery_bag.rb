e8ee5239fc30985809f04811c7a53fe0a02a6ce6
Min Suk <Kim@Mins-MBP.fios-router.home>
2018-01-24 11:48:16 -0500

    added slug methods and grocery bag model

---
+++ app/models/grocery_bag.rb
@@ -0,0 +1,11 @@
+class GroceryBag < ActiveRecord::Base
+  belongs_to :user
+
+  def slug
+    self.name.downcase.gsub(' ','-')
+  end
+
+  def self.find_by_slug(slug)
+    GroceryBag.all.find{|bag| bag.slug == slug}
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180124021805) do
+ActiveRecord::Schema.define(version: 20180124163153) do
+
+  create_table "grocery_bags", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
